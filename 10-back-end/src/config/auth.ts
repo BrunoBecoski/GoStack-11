d2c2b3085a8aedbd7c148929e909cd747e994b21
Bruno Becoski <brunobecoski@gmail.com>
2020-10-27 22:35:10 -0300

    Ajustes na API

--- 10-back-end/src/config/auth.ts
+++ 10-back-end/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: '467356b20b1a9340b1ff04caa7ce3c65',
+    secret: process.env.APP_SECRET = 'default',
     expiresIn: '1d',
   },
 };
---
+++ 10-back-end/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'email configurado no no Amazon SES',
+      name: 'name',
+    },
+  },
+} as IMailConfig;
